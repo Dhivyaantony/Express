b88d0f18298f39181fc32fd37eaee4f3fa251e06
Dhivya <dhivyaantony778@gmail.com>
2024-03-11 14:34:59 +0530

    first commit

--- app.js
+++ app.js
@@ -1,34 +1,27 @@
 const express = require('express');
 const app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello my dears');
-});
-
-app.get('/home', function (req, res) {
-    const id = req.query.id;
-    const name = req.query.name;
-
-    if (id && name) {
-        console.log({ id, name });
-        res.send('home');
-    } else {
-        res.send('Welcome to the home page');
-    }
-});
-
-// Error handling for 404 Not Found
-app.use(function(req, res, next) {
-  res.status(404).send("Sorry, can't find that!");
-});
-
-// Error handling for other errors
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
 const port = 3000;
+
+// Define a route for the home page
+app.get('/', function (req, res) {
+  res.send('Hello, this is the home page!');
+});
+
+// Define a route with a parameter
+app.get('/users/:userId', function (req, res) {
+  const userId = req.params.userId;
+  res.send(`User ID: ${userId}`);
+});
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Define a wildcard route for handling all other routes
+app.get('*', function (req, res) {
+  res.status(404).send('Page not found');
+});
+
+// Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
