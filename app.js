ad8f514b0de9e51f3a0f395b58a73db9ce636301
Dhivya <dhivyaantony778@gmail.com>
2024-03-11 14:15:29 +0530

    first commit

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
-const express = require('express')
-const app =new express()
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello my dears ')
-})
-app.get('/home/:id/:name', function (req, res) {
-    let value=req.params
-    console.log (value)
-    res.send('home')
-  })
-  
-app.listen(3000,()=>{
-    console.log("runing")
-})
+  res.send('Hello my dears');
+});
+
+app.get('/home', function (req, res) {
+    const id = req.query.id;
+    const name = req.query.name;
+
+    if (id && name) {
+        console.log({ id, name });
+        res.send('home');
+    } else {
+        res.send('Welcome to the home page');
+    }
+});
+
+// Error handling for 404 Not Found
+app.use(function(req, res, next) {
+  res.status(404).send("Sorry, can't find that!");
+});
+
+// Error handling for other errors
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
